9162a33f7d021a7223f02a20e18f6763fc8b38cf
Linus Gordon <linusgordon@Linuss-MacBook-Pro.local>
2017-04-25 11:38:12 -0400

    gfre

--- index.js
+++ index.js
@@ -335,9 +335,9 @@ function handlePostbacks(payload, sender) {
 	if (payload == "GET_STARTED_PAYLOAD") {
 	    sendTextMessage(sender, "Welcome! I will help you ask and answer questions with anyone around the world. How does that sound? :)", false);
 	} else if(payload == "ABOUT_PAYLOAD") {
-		sendTextMessage(sender, "Give or Take was developed by Linus Gordon starting April 19, 2017.\n\nFor questions, comments, or feedback, please post on http://www.facebook.com/GiveOrTakeChatbot", false);
+		sendTextMessage(sender, "Give or Take was developed by Linus Gordon starting April 19, 2017. The bot has been featured on the front page of Botlist, Qwazou, and more.\n\nFor questions, comments, or feedback, please post on http://www.facebook.com/GiveOrTakeChatbot", false);
 	} else if(payload == "STATS_PAYLOAD") {
-		sendTextMessage(sender, "The current version of Give or Take has:\n" + total_users + " users\n" + total_questions_asked + " Questions Asked\n" + total_questions_answered + " Answers Provided", false);
+		sendTextMessage(sender, "The current version of Give or Take has:\n" + total_users + " users\n" + total_questions_asked + " Questions Asked\n" + total_questions_answered + " Answers Provided\n", false);
 	} else if(payload == "HELP_PAYLOAD") {
 		sendTextMessage(sender, "Give or Take allows you to ask to ask and answer unfiltered questions with anyone on Facebook.\nIf you ask a question, I will reply with another user's answer. \nYou can also choose to answer other user's questions.", false);
 	}
