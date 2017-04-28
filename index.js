5c04f4ba672ea69912bb7364fafab4ef5520443d
Linus Gordon <linusgordon@Linuss-MacBook-Pro.local>
2017-04-28 00:09:16 -0400

    gfd

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/db', function (request, response) {
 });
 
 if (total_sent_received == 0) {
- 	for (var i = 0; i < 1; i++) {
+ 	for (var i = 0; i < initialQuestions.length; i++) {
  		client.query('INSERT INTO questions(complete, text, asker, date) VALUES (true, $1, null, null);', [initialQuestions[i]]);
  	}
 
