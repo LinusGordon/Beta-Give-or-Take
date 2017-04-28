558a073f531c46c38464ce4656f8a2c15ead8146
Linus Gordon <linusgordon@Linuss-MacBook-Pro.local>
2017-04-27 22:51:36 -0400

    hgfd

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get('/db', function (request, response) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('/db', {results: result.rows} ); }
+       { response.render('index', {results: result.rows} ); }
     });
   });
 });
