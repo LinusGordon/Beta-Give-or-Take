c02cf905562b1477c90e936856a24f044845588f
Linus Gordon <linusgordon@Linuss-MacBook-Pro.local>
2017-04-27 23:16:28 -0400

    Gfd

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/db', function (request, response) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render(path.join(__dirname+'/index.html'), {results: result.rows} ); }
+       { response.render(path.join(__dirname+'/index'), {results: result.rows} ); }
     });
   });
 });
